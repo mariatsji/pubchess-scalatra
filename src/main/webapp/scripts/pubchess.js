71d3c17d4976ad5af4aae16f83042c9c6d231707
mariatsji <sjur.millidahl@gmail.com>
2013-11-12 20:09:06 +0100

    Frontend from scratch, angular.js tutorial

---
+++ src/main/webapp/scripts/pubchess.js
@@ -0,0 +1,8 @@
+function PubchessController($scope) {
+    $scope.players = [
+        {name: 'Sjur', elo: 1200},
+        {name: 'Vasja', elo: 1300},
+        {name: 'Pixie', elo: 1100},
+        {name: 'Natalia', elo: 1300}
+    ];
+}
