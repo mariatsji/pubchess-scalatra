74fb7d9e03da8319271cb906d95ed5d2a4cf9e02
mariatsji <sjur.millidahl@gmail.com>
2013-11-12 20:38:41 +0100

    attempting players get to json api

--- src/main/webapp/scripts/pubchess.js
+++ src/main/webapp/scripts/pubchess.js
@@ -1,8 +1,7 @@
 function PubchessController($scope) {
-    $scope.players = [
-        {name: 'Sjur', elo: 1200},
-        {name: 'Vasja', elo: 1300},
-        {name: 'Pixie', elo: 1100},
-        {name: 'Natalia', elo: 1300}
-    ];
+    $scope.players = [];
+    $http.get('/players').
+    then(function (result) {
+        $scope.players = result.data;
+    });
 }
