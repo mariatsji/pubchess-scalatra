e718ae68fe935d9e0c06a9d691640f75693ac745
skvate <skvate@gmail.com>
2013-11-23 16:44:09 +0100

    test

--- src/main/webapp/scripts/players.js
+++ src/main/webapp/scripts/players.js
@@ -1,1 +1,14 @@
-//todo
+function getPlayers(){
+    var players = httpGet("http://localhost:7002/players");
+    return JSON.parse(players);
+}
+
+function httpGet(theUrl)
+{
+   var xmlHttp = null;
+
+   xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "GET", theUrl, false );
+   xmlHttp.send( null );
+   return xmlHttp.responseText;
+}
