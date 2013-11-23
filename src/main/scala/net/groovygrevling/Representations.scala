3d722f31b9802ac65e1ed7061576770f113fd7eb
mariatsji <sjur.millidahl@gmail.com>
2013-11-23 16:30:40 +0100

    Removed front-end

--- src/main/scala/net/groovygrevling/Representations.scala
+++ src/main/scala/net/groovygrevling/Representations.scala
@@ -8,7 +8,7 @@ case class Match(_id: Option[String], white_id: String, black_id: String, result
     Match(_id, white_id, black_id, result)
   }
 }
-case class Tournament(_id: Option[String], name: String, date: Option[Date], matches: List[Match])
+case class Tournament(_id: Option[String], name: String, date: Date = new Date(), matchids: List[String])
 
 object Result {
   val DRAW = 3
--- src/main/webapp/scripts/model/dataService.js
+++
@@ -1,9 +0,0 @@
-app.factory('dataService', function($http)  {
-   return {
-       getAllPlayers: function() {
-           return $http.get('/players').then(function(resp) {
-               return resp.data;
-           });
-       }
-   }
-});
